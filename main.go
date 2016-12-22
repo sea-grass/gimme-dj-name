5a18a858d1c3fdb18fc55cc44b5fe6ffce8624e6
Blender Render <dev.seagrass@gmail.com>
2016-12-22 10:08:35 -0500

    first dj name version

--- main.go
+++ main.go
@@ -1,3 +1,22 @@
 package main
 
+import (
+	"os"
+	"github.com/gin-gonic/gin"
+)
 
+func GimmeDJName(c *gin.Context) {
+	name := c.Param("name")
+	dj_name := "xx_[]**++_" + name + "_++**[]_xx"
+	c.String(http.StatusOK, dj_name)
+}
+
+func main() {
+	port := os.Getenv("PORT")
+
+	router := gin.Default()
+
+	router.GET("/xx_gimme_dat_dj_name/:name", GimmeDJName)
+
+	router.Run(port)
+}
