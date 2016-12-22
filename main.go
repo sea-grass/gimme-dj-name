d38a8b44a2749b5e6ab07c1a8e783396b248f222
Blender Render <dev.seagrass@gmail.com>
2016-12-22 10:28:12 -0500

    test

--- main.go
+++ main.go
@@ -8,6 +8,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func FirstHalf(s string) string {
+	r := NewReader(s)
+	len := Len(r)
+	//dos tuff..
+
+	return s
+}
+func SecondHalf(s string) string {
+	return ""
+}
 func main() {
 	port := os.Getenv("PORT")
 
@@ -23,6 +33,15 @@ func main() {
 	router.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "hi")
 		//c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+	router.GET("/xx_gimme_dat_dj_name/*name", func(c *gin.Context) {
+		name = gin.DefaultQuery("name", "")
+		if name == "" {
+			message := "Gotta gimme dat name doe"
+		}
+		message := "xx_**[]//_" + FirstHalf(name) + "xx" + SecondHalf(name) + "_\\[]**_xx"
+
+		c.String(http.StatusOK, message)
 	})
 
 	router.Run(":" + port)
